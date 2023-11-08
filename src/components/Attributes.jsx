82ba8eddc030917a1af25cfc0e84e456dac9b655
andressaaborges <emylleaborges@gmail.com>
2023-11-08 14:04:56 -0300

    feat: :sparkles: add new infos about the moviafro's cores

--- src/components/Attributes.jsx
+++ src/components/Attributes.jsx
@@ -31,7 +31,7 @@ const dataCardsAttributes = [
         content: {
             title: "Núcleo Moviafro de Ação Social • NUMAS",
             role: "Coordenadora: Marinalda Soares",
-            text: ""
+            text: "O NUMAS tem como finalidade primordial promover a garantia dos direitos e a proteção social de diversas parcelas da população, incluindo famílias, crianças, adolescentes, jovens, pessoas com deficiência e idosos em situação de vulnerabilidade. Para alcançar esse objetivo, atua de maneira coordenada na mobilização de esforços e recursos necessários para a execução de ações assistenciais. Essas ações abrangem aspectos preventivos, protetivos, interventivos e de inclusão social, visando assegurar a proteção integral das pessoas e seus familiares em situação de vulnerabilidade e risco pessoal e social. O NUMAS também desempenha um papel crucial ao oferecer ações socioeducativas e orientações, bem como encaminhamentos para a rede sócio-assistencial nas esferas governamentais federal, estadual e municipal. Ademais, presta suporte às entidades provedoras de serviços por meio de capacitações, monitoramentos e orientações."
         },
     },
     {
@@ -39,7 +39,7 @@ const dataCardsAttributes = [
         content: {
             title: "Núcleo Moviafro de Teatro Preto • NUMTEP",
             role: "",
-            text: ""
+            text: "Criado com o intuito de efetivar um abrangente projeto poético-político. Esse projeto engloba diversas vertentes, tais como representar o cotidiano da população negra, combater o racismo, promover a valorização e disseminação da cultura negra no país, e fomentar a participação ativa dos negros na sociedade. Além disso, busca-se estimular a conscientização e a construção das identidades negras, bem como capacitar artistas pertencentes a essa comunidade, e desenvolver uma dramaturgia e linguagem cênica próprias. O núcleo encontra-se profundamente influenciado pela rica cultura negra baiana, absorvendo elementos como a religiosidade, o caráter festivo e popular, a dança, os ritmos, os instrumentos percussivos, a memória ancestral, a estética corporal, as formas de resistência e a ênfase no registro oral da língua."
         },
     },
     {
@@ -47,15 +47,15 @@ const dataCardsAttributes = [
         content: {
             title: "Núcleo Moviafro de Apoio Jurídico • NUMAJ",
             role: "Coordenadora: Drª Diana Pereira",
-            text: ""
+            text: "Tem como finalidade primordial proporcionar atendimento especializado, com o propósito de oferecer apoio, orientação e acompanhamento às famílias que enfrentam situações de ameaça ou violação de direitos, envolvendo um ou mais de seus membros. Esse suporte engloba um conjunto de atenções e orientações direcionadas à promoção dos direitos, à preservação e ao fortalecimento dos laços familiares, comunitários e sociais. "
         },
     },
     {
         id: 7,
         content: {
-            title: "Núcleo Moviafro de Apoio ao Culto Afro-Brasileiro • NUMCAB",
+            title: "Núcleo Moviafro de Apoio ao Culto Afro-Brasileiro • NUMACAB",
             role: "Coordenadora: Ekede Patrícia Pinheiro",
-            text: ""
+            text: "Criado com o propósito de fortalecer o combate à intolerância religiosa e oferecer suporte aos centros de culto africano, em colaboração com as organizações já existentes. Além disso, tem como missão orientar adeptos e simpatizantes das religiões afro-brasileiras em uma ampla gama de temas, abrangendo desde questões de Racismo Religioso, Regularização Fundiária e Inventário, Imunidade Tributária, até Antirracismo, Associativismo e Cooperativismo, Empreendedorismo, Previdência, Estatuto da Igualdade Racial, Saúde da População Negra e Acesso aos direitos e à justiça."
         },
     },
     {
@@ -63,7 +63,7 @@ const dataCardsAttributes = [
         content: {
             title: "Núcleo Moviafro de Política Preta • NUMPP",
             role: "",
-            text: ""
+            text: "Tem como principal objetivo fortalecer o debate acerca do protagonismo negro na política partidária. Para alcançar esse propósito, o NUMPP se empenha em oferecer cursos específicos voltados para a formação política da população afrodescendente, além de prestar apoio ativo a candidaturas negras. Dessa forma, busca-se promover a participação ativa e qualificada dos afrodescendentes no cenário político, contribuindo para uma representatividade mais justa e inclusiva."
         },
     },
 ];
