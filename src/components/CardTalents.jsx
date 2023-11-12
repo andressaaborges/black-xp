9c95b1eeb84e4caf92240db58884588520faab68
andressaaborges <emylleaborges@gmail.com>
2023-11-12 01:17:47 -0300

    feat: :lipstick: update card styles
    update card styles

--- src/components/CardTalents.jsx
+++ src/components/CardTalents.jsx
@@ -3,12 +3,11 @@ import styles from './CardTalents.module.css';
 export function CardTalents({ talent, role }) {
     return (
         <section className={styles.cardTalents}>
+            <div className={styles.header}></div>
             <article className={styles.container}>
-            
                 <img src={talent.photo} alt="Foto do talento" />
-                <h1>{talent.name}<br/>{talent.surname}</h1>
+                <h1>{talent.name}<br />{talent.surname}</h1>
                 <div className={styles.dataTalents}>
-                    
                     <h4>{role}</h4>
                     <p>{talent.companies}</p>
                 </div>
