2d5af8259f076df11942723b096dbd70f527fa15
andressaaborges <emylleaborges@gmail.com>
2023-11-11 21:22:23 -0300

    refactor: :fire: remove comments

--- src/components/CardOperation.jsx
+++ src/components/CardOperation.jsx
@@ -3,7 +3,6 @@ import styles from './CardOperation.module.css';
 export function CardOperation({ number, title, description }) {
     return (
         <section className={styles.cardOperation}>
-            {/* <h1>{number}</h1> */}
             <article className={styles.container}>
                 <h1>{number}</h1>
                 <h4>{title}</h4>
