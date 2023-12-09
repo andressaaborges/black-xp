2b9795ce11c484b39272e2387d0f2e937a5fec64
andressaaborges <emylleaborges@gmail.com>
2023-12-09 14:16:36 -0300

    refactor: :speech_balloon: update texts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,8 @@
 import styles from './Footer.module.css';
 import logo from './../assets/black-xp-logo.svg';
-import { useState } from 'react';
-import { InstagramLogo, WhatsappLogo } from '@phosphor-icons/react';
+import { DiscordLogo, Envelope, InstagramLogo, WhatsappLogo } from '@phosphor-icons/react';
 
 export function Footer() {
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            let res = await fetch("api.ollo.is/subscribe_email", {
-                method: "POST",
-                headers: { "Header": "Authorization", "Bearer": "c21ca361-aec1-41ce-8099-a22fc118d70b" },
-                body: JSON.stringify({
-                    email: email,
-                }),
-            });
-            const resJson = await res.json();
-            if (resJson.status === 200) {
-                setEmail("");
-                setMessage("Email cadastrado com sucesso!");
-            } else {
-                setMessage("Ocorreu um erro.");
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
 
     return (
         <footer className={styles.footer} id="footer">
@@ -54,11 +25,11 @@ export function Footer() {
                     <img src={logo} alt="Logo Black XP" title="Logo Black XP" />
                     <h3>Ficou com alguma dúvida? Fala com a gente!</h3>
                     <div className={styles.contact}>
-                        <a href="/" target="_blank" download><p>Termos e Condições</p></a>
-                        <a href="mailto:aimotech@gmail.com" target="_blank"><p>aimotech@gmail.com</p></a>
+                        <p>Acesse as nossas redes.</p>
                         <div className={styles.social}>
-                            <a href="https://www.instagram.com/aimo.tech" target="_blank"><InstagramLogo size={30} weight="light" /></a>
-                            <a href="/" target="_blank"><WhatsappLogo size={30} weight="fill" /></a>
+                            <a href="https://www.instagram.com/blackgamejam" target="_blank"><InstagramLogo size={30} /></a>
+                            <a href="tata@aimotech.com.br" target="_blank"><Envelope size={30} /></a>
+                            <a href="https://discord.gg/Z9bSaTmh" target="_blank"><DiscordLogo size={30} /></a>
                         </div>
                     </div>
                 </div>
